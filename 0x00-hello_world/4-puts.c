9239e44e389206e6cb20a7e7171434e23dab0d35
GhitaBouchtaoui <ghita.bct@gmail.com>
2023-04-20 10:19:48 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include <stdio.h> 
+#include<stdio.h>
 /**
- *
- * Return: Always 0 (Success)
+ * main - Aprogramm that prints a line with puts function
+ * Return: 0 (Success)
  */
-int main (void)
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
